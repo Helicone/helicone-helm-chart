0209942c39c847e396cac93f1e605803d51ac19c
Erin-Boehmer <erin.boehmer@carta.com>
2023-08-08 20:57:32 -0400

    Organize cert creation

--- helm/helicone/create_tls_secrets.sh
+++ helm/helicone/create_tls_secrets.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Create tls dir
+mkdir certs
+mkdir keys
+
 # Declare arrays for different sets of values
 hosts=("worker.localhost" "helicone.localhost" "studio.localhost" "api.localhost")
 cert_files=("worker_certificate.pem" "web_certificate.pem" "studio_certificate.pem" "kong_certificate.pem")
@@ -11,12 +15,14 @@ for ((i = 0; i < ${#hosts[@]}; i++)); do
   CERT_FILE="${cert_files[i]}"
   KEY_FILE="${key_files[i]}"
 
-
   # Run the openssl command
   echo "Running openssl command for ${HOST}:"
-  openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout ${KEY_FILE} -out ${CERT_FILE} -subj "/CN=${HOST}/O=${HOST}" -addext "subjectAltName = DNS:${HOST}"
+  openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout keys/${KEY_FILE} -out certs/${CERT_FILE} -subj "/CN=${HOST}/O=${HOST}" -addext "subjectAltName = DNS:${HOST}"
   
   # Create the k8s secret
-  kubectl create secret tls ${HOST} --key ${KEY_FILE} --cert ${CERT_FILE}
+  kubectl create secret tls ${HOST} --key keys/${KEY_FILE} --cert certs/${CERT_FILE}
 
 done
+
+# Remove key files
+rm -r keys
